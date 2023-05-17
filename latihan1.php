f3febc81fee893b6d38ee0fb822fd305e950df37
REDummy <215314181@student.usd.ac.id>
2023-05-17 12:26:04 +0700

    intial commit

---
+++ latihan1.php
@@ -0,0 +1,56 @@
+<?php
+$x = 10;
+
+function printX(){
+    global $x;
+    echo $x;
+
+}
+printX();
+echo "<br>";
+echo $x;
+
+echo "<br>";
+$_GET["name"] = "mintChoco";
+$_GET["nim"] = "727";
+var_dump($_GET);
+
+$mahasiswa = [
+    [
+    "nrp" => "043040023",
+    "nama" => "Sandhika Galih",
+    "email" => "sandhikagalih@unpas.ac.id",
+    "jurusan" => "Teknik Informatika",
+    "gambar" => "sandhika.jpeg"
+    ],[
+    "nama" => "Doddy Ferdiansyah",
+    "nrp" => "033040001",
+    "email" => "doddy@gmail.com",
+    "jurusan" => "Teknik Industri",
+    "gambar" => "doddy.jpg"
+    ]
+];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>dfr</h1>
+    <ul>
+        <?php foreach($mahasiswa as $mhs) : ?>
+            <li>
+                <a href="latihan2.php?nama=<?= $mhs["nama"]; ?>&nrp=<?= $mhs["nrp"]; ?>&email=<?= $mhs["email"]; ?>&jurusan=<?= $mhs["jurusan"]; ?>"><?= $mhs["nama"]; ?></a>
+            </li>
+        <?php endforeach; ?> 
+    </ul>
+   
+</body>
+</html>
---
+++ latihan2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+<li><img src="" alt="Foto orang"></li>
+<li><?= $_GET["nama"]; ?></li>
+<li><?= $_GET["nrp"]; ?></li>
+<li><?= $_GET["email"]; ?></li>
+<li><?= $_GET["jurusan"]; ?></li>
+
+
+</ul>    
+
+<a href="latihan1.php">Return</a>
+
+</body>
+</html>
